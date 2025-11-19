6516d92992f5e3c127ae7aa80a541a819f8eafc3
PreMiD Automations <contact@premid.app>
2025-11-19 13:18:59 +0000

    chore: update assets

--- websites/S/Sooka/presence.ts
+++ websites/S/Sooka/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/UytxZSp.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/S/Sooka/assets/0.png',
 }
 
 let lastTitle: string | null = null
