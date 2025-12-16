01cd98057a709b60ff86645011bb337759ca1502
PreMiD Automations <contact@premid.app>
2025-12-16 13:00:22 +0000

    chore: update assets

--- websites/H/Hyakanime/presence.ts
+++ websites/H/Hyakanime/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/kSomZoz.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/H/Hyakanime/assets/logo.png',
 }
 
 const routes = {
