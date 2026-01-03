2983c10ebee5c03e9c0d42d838572d4fed4ba443
PreMiD Automations <contact@premid.app>
2026-01-03 22:33:21 +0000

    chore: update assets

--- websites/G/Global Player/presence.ts
+++ websites/G/Global Player/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/brgxg1I.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/G/Global%20Player/assets/logo.png',
 }
 
 let lastImageSrc: string | null = null
