e82741121507084da32bce89e0a5c239c3ef9ad9
iamhv6 <100041033+iamhv6@users.noreply.github.com>
2026-01-04 03:09:06 +0530

    feat(Kayoanime): update activity (#10265)
    * Update author ID in metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update version to 1.3.5 in metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update version number to 2.0.0 in metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Add contributors information to metadata.json
    
    Added contributors section to metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update presence data to include dynamic image fetching
    
    Fetch and set the large image key for presence data based on the meta tag.
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update presence data to include dynamic image fetching
    
    Fetch and set the large image key for presence data based on the meta tag.
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * adding contributors by adding missing newline
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Fix formatting of conditional statement for fetch_img
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update metadata.json for Kayoanime service
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Fix JSON formatting and update regex pattern
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update regex and version in metadata.json
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Fix regex pattern for Kayoanime URL
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    
    * Update websites/K/Kayoanime/metadata.json
    
    Signed-off-by: Bas van Zanten <bas.van.zanten007@gmail.com>
    
    ---------
    
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>
    Signed-off-by: Bas van Zanten <bas.van.zanten007@gmail.com>
    Co-authored-by: Bas van Zanten <bas.van.zanten007@gmail.com>

--- websites/K/Kayoanime/presence.ts
+++ websites/K/Kayoanime/presence.ts
@@ -27,6 +27,10 @@ presence.on(
   },
 )
 presence.on('UpdateData', async () => {
+
+  const fetch_img = document.querySelector<HTMLMetaElement>('meta[property="og:image"]')
+    ?.content
+
   const presenceData: PresenceData = {
     largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/K/Kayoanime/assets/logo.png',
     startTimestamp: Date.now(),
@@ -54,6 +58,11 @@ presence.on('UpdateData', async () => {
 
     default:
       if (document.querySelector('.entry-header-outer > .entry-header > h1 ')) {
+
+        if (fetch_img) {
+          presenceData.largeImageKey = fetch_img
+        }
+
         if (
           document.querySelector('.tie-fluid-width-video-wrapper')
           && everPlaying
