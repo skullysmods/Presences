096ca4b3a2eb3c1eeb0aa2c4f88ce3ae4f9aa024
Mistium <d.a.kimber@icloud.com>
2026-01-12 19:19:03 +0000

    feat(MistWarp): add activity (#10348)
    * Add MistWarp
    
    * fix metadata issue
    
    * remove comment and hopefully fix folder case issue
    
    * update metadata regex

---
+++ websites/M/mistwarp/presence.ts
@@ -0,0 +1,50 @@
+const presence = new Presence({
+  clientId: '1458217703742378044',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://raw.githubusercontent.com/MistWarp/.github/refs/heads/main/logo.png',
+}
+
+function handleEditor(presenceData: PresenceData) {
+  const projectTitleInput = document.querySelector<HTMLInputElement>('[class*="title-field"]')
+  const projectTitle = projectTitleInput?.value?.trim()
+
+  if (projectTitle) {
+    presenceData.details = `currently editing: ${projectTitle}`
+  }
+}
+
+function handleProject(presenceData: PresenceData) {
+  const projectTitle = document.querySelector<HTMLHeadingElement>('[class^="author-info_project-title_"]')?.textContent
+
+  if (projectTitle) {
+    presenceData.details = `currently playing: ${projectTitle}`
+  }
+  // add a button
+  presenceData.buttons = [
+    {
+      label: 'View Project',
+      url: window.location.href,
+    },
+  ]
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+
+  const location = new URL(window.location.href)
+
+  if (location.pathname.includes('/editor')) {
+    handleEditor(presenceData)
+  }
+  else if (location.hash.startsWith('#')) {
+    handleProject(presenceData)
+  }
+
+  presence.setActivity(presenceData)
+})
