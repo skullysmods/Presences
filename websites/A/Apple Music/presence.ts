6e58049f4322310c387be068103d87bb6d7ddc3f
Nam Anh <phamnamanh25@gmail.com>
2026-01-19 14:48:08 +0700

    feat(Apple Music): use internal metadata & add album name (#10380)
    * feat(Apple Music): use internal metadata & add album name
    
    * feat: add option to change status display text
    
    * style(manifest): consistent style casting
    
    * refactor: remove one time use constants

--- websites/A/Apple Music/presence.ts
+++ websites/A/Apple Music/presence.ts
@@ -1,4 +1,27 @@
-import { ActivityType, Assets } from 'premid'
+import { ActivityType, Assets, getTimestamps, StatusDisplayType } from 'premid'
+
+interface Attributes {
+  albumName: string
+  artistName: string
+  artwork: {
+    height: number
+    url: string
+    width: number
+  }
+  durationInMillis: number
+  name: string
+  url?: string
+}
+
+interface AudioPlayer {
+  _nowPlayingItem: {
+    attributes: Attributes
+  }
+  _currentPlaybackProgress: number
+  _paused: boolean
+  _stopped: boolean
+  _playbackDidStart: boolean
+}
 
 const presence = new Presence({
   clientId: '842112189618978897',
@@ -8,15 +31,31 @@ const strings = presence.getStrings({
   pause: 'general.paused',
 })
 
+function clearActivity() {
+  if (+presence.getExtensionVersion() < 224) {
+    presence.setActivity()
+  }
+  else {
+    presence.clearActivity()
+  }
+}
+
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/A/Apple%20Music/assets/logo.png',
   } as PresenceData
-  const [timestamps, cover, listening] = await Promise.all([
+  const [hidePaused, displayType, timestamps, cover, playback, buttons, listening] = await Promise.all([
+    presence.getSetting<boolean>('hidePaused'),
+    presence.getSetting<number>('displayType'),
     presence.getSetting<boolean>('timestamps'),
     presence.getSetting<boolean>('cover'),
+    presence.getSetting<boolean>('playback'),
+    presence.getSetting<boolean>('buttons'),
     presence.getSetting<boolean>('listening'),
   ])
+
+  let data
+
   const audio = document.querySelector<HTMLAudioElement>(
     'audio#apple-music-player',
   )
@@ -31,37 +70,70 @@ presence.on('UpdateData', async () => {
     ?.shadowRoot
     ?.querySelector('div#video-container')
     ?.querySelector<HTMLVideoElement>('video#apple-music-video-player')
-  if (video?.title || audio?.title) {
+
+  const audioPlayer = await presence.getPageVariable<{ audioPlayer: AudioPlayer }>('audioPlayer').then(res => res?.audioPlayer)
+
+  if (audioPlayer?._nowPlayingItem) {
+    const paused = audioPlayer._paused || audioPlayer._stopped || !audioPlayer._playbackDidStart
+    const { attributes } = audioPlayer._nowPlayingItem
+    const { artwork, name, artistName, albumName, durationInMillis, url } = attributes
+    const duration = durationInMillis / 1000
+
+    data = {
+      album: albumName,
+      artist: artistName,
+      artwork: artwork.url.replace('{w}', String(artwork.width)).replace('{h}', String(artwork.height)),
+      duration,
+      elapsedTime: audioPlayer._currentPlaybackProgress * duration,
+      name,
+      paused,
+      url,
+    }
+  }
+  else if (video?.title || audio?.title) {
     const media = video || audio
-    const timestamp = document
-      .querySelector('amp-lcd.lcd.lcd__music')
-      ?.shadowRoot
-      ?.querySelector<HTMLInputElement>(
-        'input#playback-progress[aria-valuenow][aria-valuemax]',
-      )
-    const paused = media && (media.paused || media.readyState <= 2)
+    const paused = !!media && (media.paused || media.readyState <= 2)
+    const metadata = navigator.mediaSession.metadata
 
-    presenceData.details = navigator.mediaSession.metadata?.title
-    presenceData.state = navigator.mediaSession.metadata?.artist
+    data = {
+      album: metadata?.album || '',
+      artist: metadata?.artist || '',
+      artwork: metadata?.artwork[0]?.src.replace(/\d{1,2}x\d{1,2}[a-z]{1,2}/, '1024x1024') || '',
+      duration: media!.duration,
+      elapsedTime: media!.currentTime,
+      name: metadata?.title || '',
+      paused,
+      url: undefined,
+    }
+  }
 
-    presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play
-    presenceData.smallImageText = paused
-      ? (await strings).pause
-      : (await strings).play
+  if (data) {
+    presenceData.details = data.name
+    presenceData.state = data.artist
 
-    if (cover) {
-      presenceData.largeImageKey = navigator.mediaSession.metadata?.artwork[0]?.src.replace(
-        /\d{1,2}x\d{1,2}[a-z]{1,2}/,
-        '1024x1024',
-      )
+    if (data.paused && hidePaused) {
+      clearActivity()
+      return
     }
 
-    [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestamps(
-      Number(timestamp ? timestamp.ariaValueNow : media!.currentTime),
-      Number(timestamp ? timestamp.ariaValueMax : media!.duration),
+    if (playback) {
+      presenceData.smallImageKey = data.paused ? Assets.Pause : Assets.Play
+      presenceData.smallImageText = data.paused
+        ? (await strings).pause
+        : (await strings).play
+    }
+
+    if (cover) {
+      presenceData.largeImageKey = data.artwork
+      presenceData.largeImageText = data.album
+    }
+
+    [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
+      data.elapsedTime,
+      data.duration,
     )
 
-    if (paused || !timestamps) {
+    if (data.paused || !timestamps) {
       delete presenceData.startTimestamp
       delete presenceData.endTimestamp
     }
@@ -69,12 +141,27 @@ presence.on('UpdateData', async () => {
     if (listening)
       presenceData.type = ActivityType.Listening
 
+    if (buttons && data.url) {
+      presenceData.buttons = [
+        {
+          label: 'Listen on Apple Music',
+          url: data.url,
+        },
+      ]
+    }
+
+    switch (displayType) {
+      case 1:
+        presenceData.statusDisplayType = StatusDisplayType.State
+        break
+      case 2:
+        presenceData.statusDisplayType = StatusDisplayType.Details
+        break
+    }
+
     presence.setActivity(presenceData)
   }
-  else if (+presence.getExtensionVersion() < 224) {
-    presence.setActivity()
-  }
   else {
-    presence.clearActivity()
+    clearActivity()
   }
 })
