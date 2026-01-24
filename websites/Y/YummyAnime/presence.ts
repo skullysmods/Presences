f53c291899789a21b36d6129afe33a6a5e14c330
Nikita Radcenko <127035335+harryys-dev@users.noreply.github.com>
2026-01-25 03:51:19 +0700

    feat(YummyAnime): add activity (#10356)
    * feat(YummyAnime): add new presence
    
    * feat(YummyAnime): add new presence
    
    * style: fix linting
    
    * style: fixes
    
    * style: fix linting errors
    
    * fix: api version
    
    Co-authored-by: Skully <117316808+skullysmods@users.noreply.github.com>
    Signed-off-by: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    
    * fix: replacing thumbnail
    
    * Update package.json
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fix: linter & import errors
    
    Co-Authored-By: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    
    * chore: revert accidentally changed package-lock.json
    
    Co-Authored-By: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    
    * Update websites/Y/YummyAnime/metadata.json
    
    Co-authored-by: Bas van Zanten <bas.van.zanten007@gmail.com>
    Signed-off-by: ch1ka <164895536+ch1kulya@users.noreply.github.com>
    
    * fix: update description to use official one & translated english version
    
    Signed-off-by: ch1ka <164895536+ch1kulya@users.noreply.github.com>
    
    * fix: valid iframe regexp
    
    Signed-off-by: ch1ka <164895536+ch1kulya@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    Signed-off-by: ch1ka <164895536+ch1kulya@users.noreply.github.com>
    Co-authored-by: Nikita Radcenko <127035335+gnice225@users.noreply.github.com>
    Co-authored-by: Skully <117316808+skullysmods@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Co-authored-by: ch1kulya <164895536+ch1kulya@users.noreply.github.com>
    Co-authored-by: Bas van Zanten <bas.van.zanten007@gmail.com>

---
+++ websites/Y/YummyAnime/presence.ts
@@ -0,0 +1,135 @@
+import { ActivityType } from 'premid'
+
+const presence = new Presence({
+  clientId: '1045800378228281345',
+})
+
+let videoData = {
+  duration: 0,
+  currentTime: 0,
+  paused: true,
+  hasData: false,
+}
+
+presence.on('iFrameData', (data: any) => {
+  videoData = { ...data, hasData: true }
+})
+
+presence.on('UpdateData', async () => {
+  const { pathname } = document.location
+
+  const presenceData: any = {
+    largeImageKey: 'https://i.imgur.com/6XnAGga.jpeg',
+    largeImageText: 'YummyAnime',
+    type: ActivityType.Watching,
+  }
+
+  if (pathname === '/' || pathname === '/index.html') {
+    presenceData.details = 'На главной странице'
+    presenceData.state = 'Выбирает аниме'
+    presence.setActivity(presenceData)
+    return
+  }
+
+  const isAnimePage = document.querySelector('.poster-block')
+
+  if (!isAnimePage) {
+    presenceData.details = 'На сайте YummyAnime'
+
+    const pageTitle = document.querySelector('h1')?.textContent?.trim()
+
+    if (pageTitle) {
+      presenceData.state = pageTitle
+    }
+    else {
+      delete presenceData.state
+    }
+
+    delete presenceData.startTimestamp
+    delete presenceData.endTimestamp
+
+    presence.setActivity(presenceData)
+    return
+  }
+
+  const titleHeader = document.querySelector('h1')
+  if (titleHeader) {
+    presenceData.details = titleHeader.textContent?.trim()
+  }
+  else {
+    presenceData.details = 'Смотрит аниме'
+  }
+
+  const posterImg = document.querySelector(
+    'div.poster-block img',
+  ) as HTMLImageElement
+  if (posterImg && posterImg.src) {
+    if (posterImg.src.startsWith('//')) {
+      presenceData.largeImageKey = `https:${posterImg.getAttribute('src')}`
+    }
+    else if (posterImg.src.startsWith('/')) {
+      presenceData.largeImageKey = `https://site.yummyani.me${posterImg.getAttribute('src')}`
+    }
+    else {
+      presenceData.largeImageKey = posterImg.src
+    }
+  }
+
+  const activeBtn = document.querySelector('div[class*="pQCG"]')
+  let currentEpisode = ''
+  if (activeBtn) {
+    const text = activeBtn.textContent?.trim()
+    if (text && !Number.isNaN(Number(text)))
+      currentEpisode = text
+  }
+
+  if (videoData.hasData) {
+    if (!videoData.paused) {
+      // === PLAY ===
+      presenceData.state = currentEpisode
+        ? `Смотрит серию: ${currentEpisode}`
+        : 'Смотрит видео'
+
+      const now = Date.now()
+      const remainingMs = (videoData.duration - videoData.currentTime) * 1000
+
+      presenceData.endTimestamp = now + remainingMs
+
+      delete presenceData.startTimestamp
+    }
+    else {
+      presenceData.state = currentEpisode
+        ? `Серия ${currentEpisode} (Пауза)`
+        : 'На паузе'
+
+      delete presenceData.startTimestamp
+      delete presenceData.endTimestamp
+    }
+  }
+  else {
+    const videoElement = document.querySelector('#video')
+    let isWatchingBlock = false
+
+    if (videoElement) {
+      const rect = videoElement.getBoundingClientRect()
+      const viewHeight = Math.max(
+        document.documentElement.clientHeight,
+        window.innerHeight,
+      )
+      if (!(rect.bottom < 0 || rect.top - viewHeight >= 0))
+        isWatchingBlock = true
+    }
+
+    if (currentEpisode && isWatchingBlock) {
+      presenceData.state = `Готовится к просмотру: ${currentEpisode}`
+    }
+    else {
+      presenceData.state = 'Читает описание'
+    }
+
+    delete presenceData.startTimestamp
+    delete presenceData.endTimestamp
+  }
+
+  presence.setActivity(presenceData)
+})
