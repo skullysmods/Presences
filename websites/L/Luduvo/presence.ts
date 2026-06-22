bd8a011cddb7454f5c137cbd21a72065ddf5b471
Aidin <69284371+mrrfyW@users.noreply.github.com>
2026-06-22 12:22:57 -0400

    fix(Luduvo): Minor fixes/additions to the forums and main site (#10915)
    * Implemented subcommunities check
    
    Also fixed up the check for normal communities as well (since they're easily described in the HTML of Discourse's forums)
    
    Signed-off-by: Aidin <69284371+mrrfyW@users.noreply.github.com>
    
    * Idk why I removed the default state for communities oops
    
    Signed-off-by: Aidin <69284371+mrrfyW@users.noreply.github.com>
    
    * Fix the quotation mark placement for topic names
    
    Signed-off-by: Aidin <69284371+mrrfyW@users.noreply.github.com>
    
    * Additional check for mobile view in the "Studio" tab
    
    Signed-off-by: Aidin <69284371+mrrfyW@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Aidin <69284371+mrrfyW@users.noreply.github.com>

--- websites/L/Luduvo/presence.ts
+++ websites/L/Luduvo/presence.ts
@@ -184,7 +184,7 @@ presence.on('UpdateData', async () => {
       }
     }
     else if (paths[0] === 'studio') {
-      const activeStudioTab = getText('button[class*="bg-accent"][class*="text-accent-foreground"] span')
+      const activeStudioTab = getText('button[class*="bg-accent"][class*="text-accent-foreground"] span') || getText('p.min-w-0.truncate.text-2xl.font-semibold')
       presenceData.state = activeStudioTab ? `Studio (${activeStudioTab})` : 'Studio'
     }
     else if (paths[0] === 'store') {
@@ -243,17 +243,21 @@ presence.on('UpdateData', async () => {
       const topicName = getText('a.fancy-title') || getText('a.topic-link span')
       const topicCreator = getText('article#post_1 .first.full-name a')
       if (topicName) {
-        presenceData.state = `Topic: "${topicName}${topicCreator ? `" by ${topicCreator}` : ''}`
+        presenceData.state = `Topic: "${topicName}"${topicCreator ? ` by ${topicCreator}` : ''}`
       }
       else {
         presenceData.state = 'Viewing a Topic'
       }
     }
     else if (paths[0] === 'c') {
-      const categorySpan = document.querySelector('span.badge-category__name')
-      const categoryName = categorySpan?.textContent?.trim()
-      if (categoryName) {
-        presenceData.state = `Category: ${categoryName}`.trim()
+      const categoryName = document.querySelector('.category-breadcrumb__category-selector .badge-category__name')?.textContent?.trim()
+      const subcategoryName = document.querySelector('.category-breadcrumb__subcategory-selector .badge-category__name')?.textContent?.trim()
+
+      if (categoryName && subcategoryName) {
+        presenceData.state = `Category: ${categoryName} (${subcategoryName})`
+      }
+      else if (categoryName) {
+        presenceData.state = `Category: ${categoryName}`
       }
       else {
         presenceData.state = 'Viewing a Category'
