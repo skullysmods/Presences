72c7dcc988d3dd700a7e21fb4e1c06c55a325de1
Evan Gregorio <107630700+randomchaffee@users.noreply.github.com>
2026-07-01 19:45:50 +0800

    feat(Witch Cult Translations): add activity (#10935)
    * feat(Witch Cult Translations): add activity
    
    * feat(Witch Cult Translations): add activity
    
    Signed-off-by: randomchaffee <evanngregorio@gmail.com>
    
    ---------
    
    Signed-off-by: randomchaffee <evanngregorio@gmail.com>

---
+++ websites/W/Witch Cult Translations/presence.ts
@@ -0,0 +1,54 @@
+const presence = new Presence({
+  clientId: '1519707327761088683',
+})
+
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+// extract chapter title from page title
+// truncate to 128 chars max
+function getChapterTitle(): string {
+  // match everything before " | Witch Cult Translations" suffix
+  const titleMatch = document.title.match(/^(.*?)\s\|\sWitch Cult Translations$/)
+  const title = titleMatch?.[1] ?? document.title
+
+  // truncate long titles and add ellipsis on it
+  return title.length > 128 ? `${title.substring(0, 125)}...` : title
+}
+
+presence.on('UpdateData', async () => {
+  const { pathname } = document.location
+
+  const i18n = await presence.getStrings({
+    browsing: 'general.browsing',
+    reading: 'general.reading',
+  })
+
+  const presenceData: PresenceData = {
+    largeImageKey: 'https://i.imgur.com/sHlFaYG.png',
+    startTimestamp: browsingTimestamp,
+  }
+
+  if (pathname === '/' || pathname === '') {
+    presenceData.details = i18n.browsing
+    presenceData.state = 'Homepage'
+  }
+  else if (pathname === '/table-of-content/' || pathname === '/table-of-content') {
+    presenceData.details = i18n.browsing
+    presenceData.state = 'Table of Contents'
+  }
+  else if (/^\/arc-\d+\/?$/.test(pathname)) {
+    const arcNumber = pathname.match(/\d+/)?.[0]
+    presenceData.details = i18n.browsing
+    presenceData.state = arcNumber ? `Arc ${arcNumber}` : 'Browsing an arc'
+  }
+  else if (/^\/\d{4}\/\d{2}\/\d{2}\/[^/]+\/?$/.test(pathname)) {
+    presenceData.details = i18n.reading
+    presenceData.state = getChapterTitle()
+  }
+  else {
+    presence.clearActivity()
+    return
+  }
+
+  presence.setActivity(presenceData)
+})
